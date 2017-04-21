89738ec1c9c15763a801ddf2bd7d386c62c4cce1
Pablo Gramajo <pgramajo@jbknowledge.com>
2017-04-21 16:38:12 -0300

    cors

--- WebApi-Aurelia/App_Start/WebApiConfig.cs
+++ WebApi-Aurelia/App_Start/WebApiConfig.cs
@@ -5,6 +5,7 @@ using System.Net.Http;
 using System.Web.Http;
 using Microsoft.Owin.Security.OAuth;
 using Newtonsoft.Json.Serialization;
+using System.Web.Http.Cors;
 
 namespace WebApi_Aurelia
 {
@@ -16,6 +17,12 @@ namespace WebApi_Aurelia
             // Configure Web API to use only bearer token authentication.
             config.SuppressDefaultHostAuthentication();
             config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
+
+
+            var urlpermitidas = new EnableCorsAttribute("*", "*", "*");
+
+            config.EnableCors(urlpermitidas);
+
 
             // Web API routes
             config.MapHttpAttributeRoutes();
